e9eadbe08b5e22c72391e3ccfe909000f468fdb8
nrth3rnlb <206222+nrth3rnlb@users.noreply.github.com>
2025-11-07 21:07:05 +0100

    refactor: Move constants and options to separate files for better organization

---
+++ replace_unwanted_characters/constants.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+DEFAULT_TAGS = ["album", "artist", "title", "albumartist", "releasetype", "label"]
+
+DEFAULT_CHAR_MAPPING = {
+    ":": "∶", "/": "⁄", "*": "∗", "?": "？", '"': '″',
+    '\\': '⧵', '.': '․', '|': 'ǀ', '<': '‹', '>': '›'
+}
